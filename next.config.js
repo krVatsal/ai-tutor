29be0e987304da8ab03da087f02f13b1979bf3c3
KrVatsal34 <kumarvatsal34@gmail.com>
2025-06-29 21:19:03 +0530

    fixing next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: { unoptimized: true },
+  
+  // Enable Server Actions for Clerk
+  experimental: {
+    serverActions: true,
+    staticPageGenerationTimeout: 120,
+  },
+
+};
 
 module.exports = nextConfig;
